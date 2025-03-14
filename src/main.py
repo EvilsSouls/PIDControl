0800cf7a6756f405bef9afb45ab6af1a1c4eafc3
EvilsSouls <evilssouls@protonmail.com>
2025-03-14 12:00:48 +0100

    Added more features to debug mode

--- src/main.py
+++ src/main.py
@@ -2,24 +2,25 @@
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, ColorSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
-#from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
-#from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.media.ev3dev import SoundFile, ImageFile, Font
 
 # Defines the starting tuning values
-BLACK = 7
-WHITE = 61
+BLACK = 12
+WHITE = 88
 TARGET_VALUE = (BLACK + WHITE) / 2 # Gets the target value by calculating what percent of light reflected would equal 50% white and 50% black (by getting the mean value between the two values)
 
-MOTOR_SPEED = 300
+MOTOR_SPEED = 30
 WHEEL_DIAMETER = 82
 AXLE_TRACK = 120
 
-PROPORTIONAL_COEFFICIENT = 1
-INTEGRAL_COEFFICIENT = 0.5
+PROPORTIONAL_COEFFICIENT = 0.5
+INTEGRAL_COEFFICIENT = 0
 DERIVATIVE_COEFFICIENT = 0
 
-debug = True
+debugMode = False
+debugDriveTime = 2000 # If Debug mode is on how many milliseconds it should drive until waiting 0 equals no waiting
 
 # Initializes starting values
 prevError = 0
@@ -34,7 +35,13 @@ motorB = Motor(Port.D)
 driveBase = DriveBase(motorA, motorB, WHEEL_DIAMETER, AXLE_TRACK)
 
 # Initializes and configures the color / light intensity sensor
-lightSensor = ColorSensor(Port.S4)
+lightSensor = ColorSensor(Port.S3)
+
+# Initializes Stopwatch
+stopWatch = StopWatch()
+
+if debugMode:
+    dataLog = DataLog(["time", "error", "prop value", "inte sum", "deri value", "turn rate"])
 
 def proportionalController(error: int, coefficient: float) -> float:
     return error * coefficient
@@ -52,13 +59,13 @@ def integralController(errorValues, cachedSum: int, newError: int, maxLength: in
     return [cachedSum, errorValues]
 
 def derivativeController(prevError: int, newError: int, coefficient: float) -> float:
-    return (prevError - newError) * coefficient
+    return (newError - prevError) * coefficient
 
-if debug:
-    i = 0
+ev3.screen.set_font(Font('Lucida', 12))
+
+stopWatch.reset()
 
 while True:
-
     lightIntensity = lightSensor.reflection()
     error = TARGET_VALUE - lightIntensity
 
@@ -73,21 +80,29 @@ while True:
 
     turnRate = proportionalValue + integralValue + derivativeValue
 
-    if(debug):
-        i += 1
-        ev3.screen.print("Iteration " + i)
-        ev3.screen.print("Prop Value: " + proportionalValue)
-        ev3.screen.print("Inte Value: " + integralValue)
-        ev3.screen.print("Deri Value: " + derivativeValue)
-        ev3.screen.print("Turn Rate: " + turnRate)
+    currentTime = stopWatch.time()
+
+    if(debugMode):
+        ev3.screen.print("Milliseconds: " + str(currentTime))
+        ev3.screen.print("Error: " + str(error))
+        ev3.screen.print("Prop Value: " + str(proportionalValue))
+        ev3.screen.print("Inte Value: " + str(integralCachedSum))
+        ev3.screen.print("Deri Value: " + str(derivativeValue))
+        ev3.screen.print("Turn Rate: " + str(turnRate))
+
+        dataLog.log([currentTime, currentTime, error, proportionalValue, integralCachedSum, derivativeValue, turnRate])
 
     # Drives the Robot
-    if not(debug):
+    if debugDriveTime > 0 and debugMode:
         driveBase.drive(MOTOR_SPEED, turnRate)
+
+        if currentTime >= debugDriveTime:
+            driveBase.stop()
+
+            # Waits for user input
+            while not(ev3.buttons.pressed()):
+                wait(1)
+            
+            stopWatch.reset()
     else:
         driveBase.drive(MOTOR_SPEED, turnRate)
-        wait(100)
-        driveBase.stop()
-        
-        while not(ev3.buttons.pressed):
-            wait(1)
